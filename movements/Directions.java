a094df823ca959d36558da9ae13407b683b98107
chema999 <jomajuba@gmail.com>
2023-07-19 14:20:49 +0200

    methods of class Directions

--- movements/Directions.java
+++ movements/Directions.java
@@ -1,13 +1,83 @@
 package movements;
 
-abstract class Directions{}
+import java.util.List;
 
-class Left extends Directions {}
+abstract class Directions{
 
-class Right extends Directions {}
+    public boolean isValid (List<List<Character>> labyrinth, int[][] position) {
 
-class Up extends Directions {}
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 2; j++) {
 
-class Down extends Directions {}
+                if (position[i][j] == -1 || labyrinth.get(i).get(j) == '#') {
 
-class Rotate extends Directions {}
+                    return false;
+                }
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+}
+
+class Left extends Directions {
+
+    public int[][] moveLeft (int[][] position) {
+            
+            for(int i = 0; i < 3; i++) {
+    
+                position[i][1]++;
+            }
+        return position;
+    }
+}
+
+class Right extends Directions {
+
+    public int[][] moveRight (int[][] position) {
+            
+            for(int i = 0; i < 3; i++) {
+    
+                position[i][1]--;
+            }
+        return position;
+    }
+}
+
+class Up extends Directions {
+
+    public int[][] moveUp (int[][] position) {
+            
+            for(int i = 0; i < 3; i++) {
+    
+                position[i][0]++;
+            }
+        return position;
+    }
+}
+
+class Down extends Directions {
+
+    public int[][] moveUp (int[][] position) {
+            
+            for(int i = 0; i < 3; i++) {
+    
+                position[i][0]--;
+            }
+        return position;
+    }
+}
+
+class Rotate extends Directions {
+
+    public int[][] rotate (int[][] position) {
+            
+          position[0][0]++;
+          position[0][1]++;
+          position[2][0]--;
+          position[2][1]--;  
+        return position;
+    }
+}
